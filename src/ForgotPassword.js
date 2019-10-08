788af99c3c4ec9a466166fd02ba284be83e2e2af
valalzarate <valeriaz@uninorte.edu.co>
2019-10-08 16:40:50 -0500

    forgot password

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -1,36 +1,37 @@
-import withRoot from './modules/withRoot';
+import withRoot from "./modules/withRoot";
 // --- Post bootstrap -----
-import React from 'react';
-import { Field, Form, FormSpy } from 'react-final-form';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from './modules/components/Typography';
-import AppFooter from './modules/views/AppFooter';
-import AppAppBar from './modules/views/AppAppBar';
-import AppForm from './modules/views/AppForm';
-import { email, required } from './modules/form/validation';
-import RFTextField from './modules/form/RFTextField';
-import FormButton from './modules/form/FormButton';
-import FormFeedback from './modules/form/FormFeedback';
+import React, { useState } from "react";
+import { Field, Form, FormSpy } from "react-final-form";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "./modules/components/Typography";
+import AppForm from "./modules/views/AppForm";
+import { email, required } from "./modules/form/validation";
+import RFTextField from "./modules/form/RFTextField";
+import FormButton from "./modules/form/FormButton";
+import FormFeedback from "./modules/form/FormFeedback";
+
+import { passwordRecovery } from "./services/firebase";
 
 const useStyles = makeStyles(theme => ({
   form: {
-    marginTop: theme.spacing(6),
+    marginTop: theme.spacing(6)
   },
   button: {
     marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(2),
+    marginBottom: theme.spacing(2)
   },
   feedback: {
-    marginTop: theme.spacing(2),
-  },
+    marginTop: theme.spacing(2)
+  }
 }));
 
 function ForgotPassword() {
   const classes = useStyles();
+
   const [sent, setSent] = React.useState(false);
 
   const validate = values => {
-    const errors = required(['email', 'password'], values);
+    const errors = required(["email", "password"], values);
 
     if (!errors.email) {
       const emailError = email(values.email, values);
@@ -42,12 +43,18 @@ function ForgotPassword() {
     return errors;
   };
 
-  const handleSubmit = () => {
+  const onSubmit = async ({ email } = {}) => {
     setSent(true);
+
+    try {
+      await passwordRecovery(email);
+    } catch (e) {
+      setSent(false);
+    }
   };
 
   return (
-    <div>    
+    <div>
       <AppForm>
         <React.Fragment>
           <Typography variant="h3" gutterBottom marked="center" align="center">
@@ -55,12 +62,19 @@ function ForgotPassword() {
           </Typography>
           <Typography variant="body2" align="center">
             {"Ingresa tu correo debajo y te enviaremos" +
-              'un link para reiniciar tu contraseña.'}
+              "un link para reiniciar tu contraseña."}
           </Typography>
         </React.Fragment>
-        <Form onSubmit={handleSubmit} subscription={{ submitting: true }} validate={validate}>
-          {({ handleSubmit2, submitting }) => (
-            <form onSubmit={handleSubmit2} className={classes.form} noValidate>
+        <Form onSubmit={() => {}} validate={validate}>
+          {({ submitting, values }) => (
+            <form
+              onSubmit={ev => {
+                ev.preventDefault();
+                onSubmit(values);
+              }}
+              className={classes.form}
+              noValidate
+            >
               <Field
                 autoFocus
                 autoComplete="email"
@@ -89,13 +103,15 @@ function ForgotPassword() {
                 color="secondary"
                 fullWidth
               >
-                {submitting || sent ? 'Procesando…' : 'Enviar link de recuperación'}
+                {submitting || sent
+                  ? "Procesando…"
+                  : "Enviar link de recuperación"}
               </FormButton>
             </form>
           )}
         </Form>
       </AppForm>
-      </div>
+    </div>
   );
 }
 
