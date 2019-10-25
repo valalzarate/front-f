b5901662aa45c8c2c219a4a9d54b85051980aaee
Maher Stehisy <36872402+MaherStehisy@users.noreply.github.com>
2019-10-24 20:42:34 -0500

    Add files via upload

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/database";
 import "firebase/storage";
 import "firebase/auth";
+import "firebase/firestore"
 
 firebase.initializeApp({
   apiKey: "AIzaSyCyyqZlfpRRa8lZ4VmqbyhWM2jKPQ8fIOU",
@@ -14,6 +15,7 @@ firebase.initializeApp({
 });
 
 export const auth = firebase.auth();
+export const db = firebase.firestore();
 
 export const login = (email, password) => {
   return auth.signInWithEmailAndPassword(email, password);
